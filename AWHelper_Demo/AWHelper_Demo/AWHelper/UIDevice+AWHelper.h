78c3851606a0d113544ca20615d96ab278824ed3
AlanWong <AlanWong@hebao032.local>
2015-10-26 15:47:48 +0800

    【修改】添加系统版本的宏

--- AWHelper_Demo/AWHelper_Demo/AWHelper/UIDevice+AWHelper.h
+++ AWHelper_Demo/AWHelper_Demo/AWHelper/UIDevice+AWHelper.h
@@ -7,6 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#define IOS9_OR_LATER	( [[[UIDevice currentDevice] systemVersion] compare:@"9.0"] != NSOrderedAscending )
+#define IOS8_OR_LATER	( [[[UIDevice currentDevice] systemVersion] compare:@"8.0"] != NSOrderedAscending )
+#define IOS7_OR_LATER	( [[[UIDevice currentDevice] systemVersion] compare:@"7.0"] != NSOrderedAscending )
+#define IOS6_OR_LATER	( [[[UIDevice currentDevice] systemVersion] compare:@"6.0"] != NSOrderedAscending )
+#define IOS5_OR_LATER	( [[[UIDevice currentDevice] systemVersion] compare:@"5.0"] != NSOrderedAscending )
+#define IOS4_OR_LATER	( [[[UIDevice currentDevice] systemVersion] compare:@"4.0"] != NSOrderedAscending )
+#define IOS3_OR_LATER	( [[[UIDevice currentDevice] systemVersion] compare:@"3.0"] != NSOrderedAscending )
 
 typedef NS_ENUM(NSUInteger ,AWDeviceType){
     // 未知设备类型
